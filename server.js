55d79abda24b8dc35762570f0298697c253e29de
Ruben Gil <gil.r419@gmail.com>
2016-12-20 18:01:32 -0500

    completed user tests and routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyparser = require('body-parser')
 const app = express();
 const User = require('./models/user-model');
+const Song = require('./models/song-model');
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
 
 app.get('/', (req, res) => {
   res.send('hello world!');
@@ -12,6 +17,41 @@ app.get('/users', (req, res) => {
     res.send(data);
   })
 });
+
+app.get('/users/id/:id', (req, res) => {
+  User.findById(req.params.id)
+  .then((data) => {
+    res.send(data);
+  })
+})
+
+app.get('/users/username/:username', (req, res) => {
+  User.findAll({where: {username: req.params.username}})
+  .then((data) => {
+    res.send(data);
+  })
+})
+
+app.get('/users/sort/a-z', (req, res) => {
+  User.findAll({order: "username"})
+  .then((data) => {
+    res.send(data);
+  })
+})
+
+app.post('/users', (req, res) => {
+  User.create(req.body)
+  .then((user) => {
+    res.send(user);
+  })
+})
+
+app.get('/songs', (req, res) => {
+  Song.findAll()
+  .then((data) => {
+    res.send(data);
+  })
+})
 
 
 module.exports = app;
--- test/user-tests.js
+++ test/user-tests.js
@@ -10,6 +10,10 @@ describe('User tests', () => {
     {username: 'test2', email: 'test2@gmail.com', password: 'pass2'},
     {username: 'test3', email: 'test3@gmail.com', password: 'pass3'},
   ];
+
+  //chooses a random user id
+  let id = Math.floor(Math.random() * users.length) + 1;
+
   //you can use 'before' to seed your database with data before your tests
   //you only need one 'before' statement
   //theres also a 'beforeEach' method if you want a function to run before each of your tests, individually
@@ -42,4 +46,55 @@ describe('User tests', () => {
         done();
       })
   });
-});
+
+  //test to get a single user by id
+  it(`'/users/id/:id' should respond with an individual user`, (done) => {
+    supertest(server)
+      .get(`/users/id/${id}`)
+      .end((err, res) => {
+        expect(res.body.id).equal(id);
+        done();
+      })
+  })
+
+  //test to get a single user by username
+  it(`'/users/username/:username' should get a user by username`, (done) => {
+    let username = users[id-1].username;
+
+    supertest(server)
+      .get(`/users/username/${username}`)
+      .end((err, res) => {
+        expect(res.body.length).equal(1);
+        expect(res.body[0].username).equal(username);
+        done();
+      })
+  })
+
+  //test to get all users sorted by username
+  it(`'/users/sort/a-z' should get all users sorted by username`, (done) => {
+    supertest(server)
+      .get('/users/sort/a-z')
+      .end((err, res) => {
+        expect(res.body.length).equal(3);
+        expect(res.body[0].username).equal(users[0].username);
+        expect(res.body[1].username).equal(users[1].username);
+        expect(res.body[2].username).equal(users[2].username);
+        done();
+      })
+  });
+
+  it(`/users should create a new user`, (done) => {
+    let newUser = {username: 'test4', email: 'test4@gmail.com', password: 'pass4'};
+
+    supertest(server)
+      .post('/users')
+      .send(newUser)
+      .end((err, res) => {
+        expect(res.body.username).equal(newUser.username);
+        expect(res.body.email).equal(newUser.email);
+        expect(res.body.password).equal(newUser.password);
+        done();
+      })
+  })
+
+})
