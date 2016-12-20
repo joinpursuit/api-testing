175ac5a427bb882d08e766ba99bb5fb1a3177f10
iliass <iliass24@gmail.com>
2016-12-20 18:35:03 -0500

    added songs route testing

--- models/song-model.js
+++ models/song-model.js
@@ -1,11 +1,22 @@
 const Sequelize = require('sequelize');
 const sequelizeConnection = require('../db');
+const User = require('./user-model');
 
-//Your code here:
+var Song = sequelizeConnection.define('song', {
+  title: {
+    type: Sequelize.STRING,
+    validate: {
+      len: [1, 50]
+    }
+  },
+  artist: {
+    type: Sequelize.STRING,
+    validate: {
+      len: [1, 100]
+    }
+  }
+});
 
+Song.belongsTo(User);
 
-
-
-
-
-//be sure to export your model as Song
+module.exports = Song;
