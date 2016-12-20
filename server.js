c8db6703e8746e501c08e02ed5a4a45c55b2304d
Joshua Fermin <Joshuaf91@gmail.com>
2016-12-20 16:53:27 -0500

    built api route for /user/username/:username

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ const app = express();
 const User = require('./models/user-model');
 
 app.get('/', (req, res) => {
-  res.send('hello world!');
+    res.send('hello world!');
 });
 
 app.get('/users', (req, res) => {
-  User.findAll()
-  .then((data) => {
-    res.send(data);
-  })
+    User.findAll()
+        .then((data) => {
+            res.send(data);
+        })
 });
+
+app.get('/user/username/:username', (req, res) => {
+	console.log(req.params.username)
+    User.findOne({ where: 
+    		{ username: req.params.username } 
+    	})
+    	.then(data=>{
+    		res.send(data);
+    	})
+})
 
 
 module.exports = app;
